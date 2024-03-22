045b7d0aafb9e67465f3b8717b574ee680807838
jim <jimc@bethel.com.tw>
2024-03-22 08:55:58 +0800

    2024-03-22 09:00

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:text_to_speech/text_to_speech.dart';
+import 'package:./sport/tts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,70 +14,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final String defaultLanguage = 'en-US';
-
-  TextToSpeech tts = TextToSpeech();
-
-  String text = 'Hello, every one';
-  double volume = 1; // Range: 0-1
-  double rate = 1.0; // Range: 0-2
-  double pitch = 1.0; // Range: 0-2
-
-  String? language;
-  String? languageCode;
-  List<String> languages = <String>[];
-  List<String> languageCodes = <String>[];
-  String? voice;
-
-  TextEditingController textEditingController = TextEditingController();
+  TTS tts = TTS(); 
 
   @override
   void initState() {
     super.initState();
-    text = "one";
-    textEditingController.text = text;
-    WidgetsBinding.instance?.addPostFrameCallback((_) {
-      initLanguages();
+  
+    WidgetsBinding.instance?.addPostFrameCallback((_) async {
+      await tts.initial();
     });
   }
 
-  Future<void> initLanguages() async {
-    /// populate lang code (i.e. en-US)
-    languageCodes = await tts.getLanguages();
-
-    /// populate displayed language (i.e. English)
-    final List<String>? displayLanguages = await tts.getDisplayLanguages();
-    if (displayLanguages == null) {
-      return;
-    }
-
-    languages.clear();
-    for (final dynamic lang in displayLanguages) {
-      languages.add(lang as String);
-    }
-
-    final String? defaultLangCode = "en-US"; // await tts.getDefaultLanguage();
-    if (defaultLangCode != null && languageCodes.contains(defaultLangCode)) {
-      languageCode = defaultLangCode;
-    } else {
-      languageCode = defaultLanguage;
-    }
-    language = await tts.getDisplayLanguageByCode(languageCode!);
-
-    /// get voice
-    voice = await getVoiceByLang(languageCode!);
-
-    if (mounted) {
-      setState(() {});
-    }
-  }
-
-  Future<String?> getVoiceByLang(String lang) async {
-    final List<String>? voices = await tts.getVoiceByLang(languageCode!);
-    if (voices != null && voices.isNotEmpty) {
-      return voices.first;
-    }
-    return null;
+  speak() {
+    tts.speak("");
   }
 
   @override
@@ -93,125 +42,7 @@ class _MyAppState extends State<MyApp> {
             child: Center(
               child: Column(
                 children: <Widget>[
-                  TextField(
-                    controller: textEditingController,
-                    maxLines: 5,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter some text here...'),
-                    onChanged: (String newText) {
-                      setState(() {
-                        text = newText;
-                      });
-                    },
-                  ),
-                  Row(
-                    children: <Widget>[
-                      const Text('Volume'),
-                      Expanded(
-                        child: Slider(
-                          value: volume,
-                          min: 0,
-                          max: 1,
-                          label: volume.round().toString(),
-                          onChanged: (double value) {
-                            initLanguages();
-                            setState(() {
-                              volume = value;
-                            });
-                          },
-                        ),
-                      ),
-                      Text('(${volume.toStringAsFixed(2)})'),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      const Text('Rate'),
-                      Expanded(
-                        child: Slider(
-                          value: rate,
-                          min: 0,
-                          max: 2,
-                          label: rate.round().toString(),
-                          onChanged: (double value) {
-                            setState(() {
-                              rate = value;
-                            });
-                          },
-                        ),
-                      ),
-                      Text('(${rate.toStringAsFixed(2)})'),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      const Text('Pitch'),
-                      Expanded(
-                        child: Slider(
-                          value: pitch,
-                          min: 0,
-                          max: 2,
-                          label: pitch.round().toString(),
-                          onChanged: (double value) {
-                            setState(() {
-                              pitch = value;
-                            });
-                          },
-                        ),
-                      ),
-                      Text('(${pitch.toStringAsFixed(2)})'),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      const Text('Language'),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      DropdownButton<String>(
-                        value: language,
-                        icon: const Icon(Icons.arrow_downward),
-                        iconSize: 24,
-                        elevation: 16,
-                        style: const TextStyle(color: Colors.deepPurple),
-                        underline: Container(
-                          height: 2,
-                          color: Colors.deepPurpleAccent,
-                        ),
-                        onChanged: (String? newValue) async {
-                          languageCode =
-                              await tts.getLanguageCodeByName(newValue!);
-                          voice = await getVoiceByLang(languageCode!);
-                          setState(() {
-                            language = newValue;
-                          });
-                        },
-                        items: languages
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      const Text('Voice'),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      Text(voice ?? '-'),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
+                
                   Row(
                     children: <Widget>[
                       Expanded(
@@ -220,35 +51,11 @@ class _MyAppState extends State<MyApp> {
                           child: ElevatedButton(
                             child: const Text('Stop'),
                             onPressed: () {
-                              tts.stop();
+                              // tts.stop();
                             },
                           ),
                         ),
                       ),
-                      if (supportPause)
-                        Expanded(
-                          child: Container(
-                            padding: const EdgeInsets.only(right: 10),
-                            child: ElevatedButton(
-                              child: const Text('Pause'),
-                              onPressed: () {
-                                tts.pause();
-                              },
-                            ),
-                          ),
-                        ),
-                      if (supportResume)
-                        Expanded(
-                          child: Container(
-                            padding: const EdgeInsets.only(right: 10),
-                            child: ElevatedButton(
-                              child: const Text('Resume'),
-                              onPressed: () {
-                                tts.resume();
-                              },
-                            ),
-                          ),
-                        ),
                       Expanded(
                           child: Container(
                         child: ElevatedButton(
@@ -269,29 +76,4 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  bool get supportPause => defaultTargetPlatform != TargetPlatform.android;
-
-  bool get supportResume => defaultTargetPlatform != TargetPlatform.android;
-
-  void speak() async {
-    tts.setVolume(volume);
-    tts.setRate(rate);
-    if (languageCode != null) {
-      tts.setLanguage(languageCode!);
-    }
-    tts.setPitch(pitch);
-    // tts.speak(text);
-    var arr = ["1", "2", "3", "4", "over"];
-
-    for(var i = 0; i < 100; i++) {
-       tts.speak(i.toString());
-      await waitTask();
-    }
-   
-  }
-
-  Future waitTask() async {
-  await Future.delayed(const Duration(seconds: 1));
-  return;
-}
 }
--- lib/tts.dart
+++ lib/tts.dart
@@ -1,51 +1,30 @@
 import 'package:text_to_speech/text_to_speech.dart';
-
 // zh-TW, en-US
-
-// https://www.jianshu.com/p/8d86ba3b423a
-// https://pub.dev/packages/flutter_tts/example
 // https://pub.dev/packages/text_to_speech/install
 
-class TextToSpseech {
+class TTS {
   TextToSpeech tts = TextToSpeech();
-  double volume = 1; // Range: 0-1
-  double rate = 1.0; // Range: 0-2
-  double pitch = 1.0; // Range: 0-2
-  String? language;
-  String? languageCode;
-  List<String> languages = <String>[];
-  List<String> languageCodes = <String>[];
-  String? voice;
-
 
   Future<void> initial() async {
     /// populate lang code (i.e. en-US)
-    languageCodes = await tts.getLanguages();
-
-    /// populate displayed language (i.e. English)
-    final List<String>? displayLanguages = await tts.getDisplayLanguages();
-    if (displayLanguages == null) {
-      return;
-    }
-
-    languages.clear();
-    for (final dynamic lang in displayLanguages) {
-      languages.add(lang as String);
-    }
-
-    final String? defaultLangCode = "en-US"; // await tts.getDefaultLanguage();
-    
-    language = await tts.getDisplayLanguageByCode(languageCode!);
-
-    // voice = await getVoiceByLang(languageCode!);
-
-
+    tts.setLanguage("en-US");
   }
+
 
 
   Future speak(String text) async {
     // tts.setVolume(1);  
     // String text = "Hello, Good Morning!";  
-    // tts.speak(text);  
+    // tts.speak(text);
+
+    for(var i = 0; i < 100; i++) {
+       tts.speak(i.toString());
+      await waitTask();
+    }
+  }
+
+  Future waitTask() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return;
   }
 }
