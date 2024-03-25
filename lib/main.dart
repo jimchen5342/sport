0b3faadf9cbb1dc2b647b1945b8de3d363e7ebff
jim <jimc@bethel.com.tw>
2024-03-25 15:38:02 +0800

    2024-03-25 16:00

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:math';
-
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sensors/flutter_sensors.dart';
 import 'package:./sport/tts.dart';
@@ -17,11 +17,12 @@ class _MyAppState extends State<MyApp> {
   List<double> _accelData = List.filled(3, 0.0);
   StreamSubscription? _accelSubscription;
   int swingCount = 0;
-  double acceleration = 0;
+  int acceleration = 0;
 
-  int SHAKE_SLOP_TIME_MS = 500;
   int mShakeTimestamp = 0;
   TTS tts = TTS();
+
+  String recorders = "";
   
   @override
   void initState() {
@@ -29,7 +30,7 @@ class _MyAppState extends State<MyApp> {
     WidgetsBinding.instance?.addPostFrameCallback((_) async {
       await tts.initial();
       _checkAccelerometerStatus();
-      tts.count();
+      // tts.count();
     });
   }
 
@@ -53,6 +54,7 @@ class _MyAppState extends State<MyApp> {
     if (_accelSubscription != null) return;
     if (_accelAvailable) {
       swingCount = 0;
+      recorders = "";
       final stream = await SensorManager().sensorUpdates(
         sensorId: Sensors.ACCELEROMETER,
         interval: Sensors.SENSOR_DELAY_UI, //  Sensors.SENSOR_DELAY_FASTEST,
@@ -67,27 +69,24 @@ class _MyAppState extends State<MyApp> {
   }
 
   void onSensorChanged(List<double> event) {
-        // 檢測加速度變化，這裡可以自行調整閾值
         double x = event[0];
         double y = event[1];
         double z = event[2];
-        double xyz = sqrt(x*x + y*y + z*z);
+        int xyz = sqrt(x*x + y*y + z*z).ceil();
 
-        // // 如果檢測到手擺動，增加手擺動次數
-        // if (acceleration > 15) {  // 這個閾值可能需要調整
-        //     swingCount++;
-        //     // tts.speak(swingCount.toString());
-        // }
-        
         if (acceleration < xyz && acceleration > 15) {
           int now = DateTime.now().millisecondsSinceEpoch;
-          if (mShakeTimestamp + 300 < now) {
+          if (mShakeTimestamp + 500 < now) {
             swingCount++;
             if(swingCount % 5 == 0) {
               tts.speak(swingCount.toString());
             }
+            String formattedDate = DateFormat('mm:ss.ms').format(DateTime.now());
+
+            recorders = formattedDate + ": " + acceleration.toString() 
+              + (recorders.isNotEmpty ? "\n" : "") + recorders;
+            mShakeTimestamp = now;
           }
-          mShakeTimestamp = now;
         }
         acceleration = xyz;
     }
@@ -111,25 +110,25 @@ class _MyAppState extends State<MyApp> {
           alignment: AlignmentDirectional.topCenter,
           child: Column(
             children: <Widget>[
-              Text(
-                "Accelerometer Enabled: $_accelAvailable",
-                textAlign: TextAlign.center,
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Text(
-                "X = ${_accelData[0]}",
-                textAlign: TextAlign.center,
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Text(
-                "Y = ${_accelData[1]}",
-                textAlign: TextAlign.center,
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Text(
-                "Z = ${_accelData[2]}",
-                textAlign: TextAlign.center,
-              ),
+              // Text(
+              //   "Accelerometer Enabled: $_accelAvailable",
+              //   textAlign: TextAlign.center,
+              // ),
+              // Padding(padding: EdgeInsets.only(top: 16.0)),
+              // Text(
+              //   "X = ${_accelData[0]}",
+              //   textAlign: TextAlign.center,
+              // ),
+              // Padding(padding: EdgeInsets.only(top: 16.0)),
+              // Text(
+              //   "Y = ${_accelData[1]}",
+              //   textAlign: TextAlign.center,
+              // ),
+              // Padding(padding: EdgeInsets.only(top: 16.0)),
+              // Text(
+              //   "Z = ${_accelData[2]}",
+              //   textAlign: TextAlign.center,
+              // ),
                Text(
                 "acceleration = ${acceleration}",
                 textAlign: TextAlign.center,
@@ -137,6 +136,15 @@ class _MyAppState extends State<MyApp> {
               Text(
                 "swingCount = ${swingCount}",
                 textAlign: TextAlign.center,
+              ),
+              Text(
+                recorders,
+                maxLines: 30,
+                textAlign: TextAlign.center,
+              ),
+              Expanded(
+                flex: 1, 
+                child: Container()
               ),
               Padding(padding: EdgeInsets.only(top: 16.0)),
               Row(
@@ -148,9 +156,7 @@ class _MyAppState extends State<MyApp> {
                     onPressed:
                         _accelAvailable ? () => _startAccelerometer() : null,
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                  ),
+                  Padding(padding: EdgeInsets.all(8.0)),
                   MaterialButton(
                     child: Text("Stop"),
                     color: Colors.red,
