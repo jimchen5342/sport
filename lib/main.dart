e2ec018645ad39582b0ad30b24f8a7b6b72fad7f
jim <jimc@bethel.com.tw>
2024-03-29 09:07:30 +0800

    2024-03-29

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,6 @@ class _HomePage extends StatefulWidget {
   State<_HomePage> createState() => _HomePageState();
 }
 
-// 
 class _HomePageState extends State<_HomePage> {
   List<dynamic> list = [];
 
@@ -62,6 +61,22 @@ class _HomePageState extends State<_HomePage> {
     WidgetsBinding.instance?.addPostFrameCallback((_) async {
       // await Storage.clear();
       list = await Storage.getJSON("swing");
+      // list.add({"date": "2024-03-20", "left": 1, "right": 1});
+      // list.insert(0, {"date": "2024-03-10", "left": 11, "right": 11});
+      // list.add({"date": "2024-03-20", "left": 1, "right": 1});
+      // list.insert(0, {"date": "2024-03-10", "left": 11, "right": 11});
+      // list.add({"date": "2024-03-20", "left": 1, "right": 1});
+      // list.insert(0, {"date": "2024-03-10", "left": 11, "right": 11});
+      // list.add({"date": "2024-03-20", "left": 1, "right": 1});
+      // list.insert(0, {"date": "2024-03-10", "left": 11, "right": 11});
+      // list.add({"date": "2024-03-20", "left": 1, "right": 1});
+      // list.insert(0, {"date": "2024-03-10", "left": 11, "right": 11});
+      // list.add({"date": "2024-03-20", "left": 1, "right": 1});
+      // list.insert(0, {"date": "2024-03-10", "left": 11, "right": 11});
+      // await Storage.setJSON("swing", list);
+
+      setState(() {});
+      // print(list);
     });
   }
 
@@ -84,18 +99,90 @@ class _HomePageState extends State<_HomePage> {
           // actions: [Text("First action")],
           backgroundColor: Colors.blue, 
         ),
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.all(5),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/swing');
-              },
-              child: const Text('Next page'),
+            Expanded(
+              flex: 1, 
+              child: ListView.builder(
+                itemCount: list.length,
+                // itemExtent: 50.0, //强制高度为50.0
+                itemBuilder: (BuildContext context, int index) {
+                  return rowRender(index); // ListTile(title: Text("$index"));
+                }
+              )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                  MaterialButton(
+                    padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                    child:  Text("運動",
+                      style: TextStyle(
+                        color:Colors.white,
+                        fontSize: 18
+                      )
+                    ),
+                    color: Colors.blue,
+                    onPressed:() {
+                       Navigator.of(context).pushNamed('/swing');
+                    }
+                  ),
+              ],
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget rowRender(int index) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Row(
+        children: [
+          SizedBox(width: 25,
+            child: Text((index + 1).toString(),
+            style: const TextStyle(
+              fontSize: 16
+            ),  
+            // textAlign: TextAlign.center,
+          ),
+          ),
+          Column(
+            children: [
+              Text(list[index]["date"],
+                style: const TextStyle(
+                  fontSize: 16
+                ),  
+                // textAlign: TextAlign.center,
+              ),
+              Row(
+                children: [
+                  // Expanded( flex: 1,  
+                  //   child: Text("左：" + list[index]["left"].toString(),
+                  //     style: const TextStyle(
+                  //       fontSize: 16
+                  //     )
+                  //   )
+                  // ),
+                  // Expanded( flex: 1,  
+                  //   child: Text("右：" + list[index]["right"].toString(),
+                  //     style: const TextStyle(
+                  //       fontSize: 16
+                  //     )
+                  //   )
+                  // ),
+                ]
+              )
+            ]
+          )
+        ]
       ),
     );
   }
